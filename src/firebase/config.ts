ff21e1e093bf22f6d0065f5c0c80d0ee08646b47
Suraj Kamath <operationsupport@malpemeen.com>
2025-11-13 05:14:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8151986180-3e97c",
+  "appId": "1:575026927801:web:1f272a11824581cb322368",
+  "apiKey": "AIzaSyAQMUR4NJYhRTrAjMl7Er9392ms9aRUfBk",
+  "authDomain": "studio-8151986180-3e97c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "575026927801"
+};
