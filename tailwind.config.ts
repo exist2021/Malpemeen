857c1bc1fbb23eb2d4323681432e9af8d22c5ae3
Suraj Kamath <operationsupport@malpemeen.com>
2025-11-19 11:31:57 +0000

    Try to design background similar to uploaded image

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,7 @@ export default {
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
+        glass: 'rgba(255, 255, 255, 0.1)',
         card: {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
