88e1d728039b49bb42d1937776da7f9367eb72d2
Suraj Kamath <operationsupport@malpemeen.com>
2025-11-21 06:58:38 +0000

    https://tse2.mm.bing.net/th/id/OIP.Z9Udy9KlJcw4DbHwtLGTowHaHa?rs=1&pid=I

--- src/components/fish-logo.tsx
+++ src/components/fish-logo.tsx
@@ -6,7 +6,7 @@ export function FishLogo({ className }: { className?: string }) {
   return (
     <div className={cn("relative", className)}>
       <Image 
-        src="/logo.png" 
+        src="https://tse2.mm.bing.net/th/id/OIP.Z9Udy9KlJcw4DbHwtLGTowHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" 
         alt="Malpe Meen Logo"
         fill
         className="object-contain"
