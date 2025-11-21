00731a5a2fcf06b22bd993989a644d89839181b3
Suraj Kamath <operationsupport@malpemeen.com>
2025-11-21 07:05:24 +0000

    make logo transp[arent

--- src/components/fish-logo.tsx
+++ src/components/fish-logo.tsx
@@ -5,8 +5,8 @@ import { cn } from "@/lib/utils"
 export function FishLogo({ className }: { className?: string }) {
   return (
     <div className={cn("relative", className)}>
-      <Image 
-        src="https://tse2.mm.bing.net/th/id/OIP.Z9Udy9KlJcw4DbHwtLGTowHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" 
+      <Image
+        src="https://tse2.mm.bing.net/th/id/OIP.Z9Udy9KlJcw4DbHwtLGTowHaHa?rs=1&pid=ImgDetMain&o=7&rm=3"
         alt="Malpe Meen Logo"
         fill
         className="object-contain"
@@ -14,3 +14,4 @@ export function FishLogo({ className }: { className?: string }) {
     </div>
   );
 }
+
