07c1e59ff92d60cfb894c174b3f6a3f17be12cc1
Suraj Kamath <operationsupport@malpemeen.com>
2025-12-01 10:34:58 +0000

    when user adds published link to home layout bookmark that icon should b

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+       <head>
+        <link rel="icon" href="https://tse2.mm.bing.net/th/id/OIP.Z9Udy9KlJcw4DbHwtLGTowHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased relative",
